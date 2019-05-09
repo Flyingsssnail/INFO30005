d27f02bbbce665a53d9904205979a4f5c32aabe5
WHATTTAREFUN <junlongw@student.unimelb.edu.au>
2019-05-09 16:20:58 +1000

    updated routers functional

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controller/controller.js');
+var mongoose  = require ('mongoose');
+var Users = mongoose.model('users');
+var Posts = mongoose.model('posts');
 
 // welcome page
 router.get('/', controller.init);
@@ -19,6 +22,25 @@ router.post('/api/post', controller.createPost);
 
 // router.get('/api/post', controller.getPost);
 // router.get('/api/post_edit', controller.editPost);
+router.get('/login', controller.finduser);
+   /* function(req, res) {
+    console.log('hi');
+    var user = new Users();
+    Users.find({name: req.query.name}, function(err, result){
+        return err ? res.sendStatus(404) :
+        res.render('login', { message: 'Hello world!', names: [ { name: "steven"}, { name: "Daniel"}],result: result });
+    });*/
+
+    // Make DB request
+    // User.findById(req.params.id, function(err, user) {
+    //    res.render('profile', user)
+    // });
+    // var user = ...
+
+
+
+  // res.render('login', { message: 'Hello world!', names: [ { name: "steven"}, { name: "Daniel"} ] });
+//});
 
 // router.get('/api/login', controller.login);
 
